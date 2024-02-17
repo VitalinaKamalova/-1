2ce185e4d320db8793ef966a51f1c7b2df9d3d81
Vitalina <151424991+VitalinaKamalova@users.noreply.github.com>
2024-02-17 14:07:04 +1000

    laba0.c

--- laba0.c
+++ laba0.c
@@ -1,37 +1,37 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-int main() {
-    
-    double a,b,c,d,x1,x2;
 
-    printf("Vvedite a:");
-    scanf("%lf",&a);
-    printf("Vvedite b:");
-    scanf("%lf",&b);
-    printf("Vvedite c:");
-    scanf("%lf",&c);
-
-    d= b*b - 4*a*c;
-    printf("d = %lf", d);
-
-if(d>0) {
-        
-    x1 = (-b+sqrt(d))/(2*a);
-    x2 = (-b-sqrt(d))/(2*a);
+int main()
+{
+    double a, b, c, D=0, x1, x2;
+    int x;
+    do {
     
-    printf("x1 = %lf x2 = %lf \n", x1,x2);
-}
-if (d==0) {
-    x1 = (-b/(2*a));
-    printf("x1 = %lf \n", x1);
-}
-if (d<0) {
-    printf("Korney net \n");
+    printf("Vvedite a: ");
+    scanf("%lf", &a);
+    printf("Vvedite b: ");
+    scanf("%lf", &b);
+    printf("Vvedite c: ");
+    scanf("%lf", &c);
     
-}
+    D = b*b - 4*a*c;
+    printf("D = %lf", D);
     
-    printf("Hello World");
-
+    if (D>0) {
+        x1=(-b+sqrt(D))/(2*a);
+        x2=(-b-sqrt(D))/(2*a);
+    printf("x1=%lf x2=%lf", x1, x2);
+     
+    }
+    if (D==0) {
+        x1=(-b/(2*a));
+    }
+    if (D<0) {
+        printf("Корней нет");
+    }
+    printf("Для повторения работы программы нажмите 1: ");
+    scanf("%d", &x); 
+    } while(x==1);
     return 0;
 }
