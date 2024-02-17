ab8506e4afe7bd2ef9b55f50678d6fae9d0c6a68
Vitalina <151424991+VitalinaKamalova@users.noreply.github.com>
2024-02-17 16:35:50 +1000

    Create laba2.c

---
+++ laba2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   
+    int i, j;
+    int N;
+    
+    printf("Введите значения размнерность: ");
+    scanf("%d", &N);
+    double array[N][N];
+    
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++)
+        {
+            printf("введите значения array[%d][%d]: ", i+1, j+1);
+            scanf("%lf", &array[i][j]);
+        }
+    }
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++)
+        {
+            printf("%lf ",array[i][j]);
+        }
+    printf("\n");
+    }
+}
